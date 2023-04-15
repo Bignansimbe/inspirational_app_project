5ba760a1cc53e4819a02736e8413970983bd3428
Bignansimbe <126199260+Bignansimbe@users.noreply.github.com>
2023-04-15 00:53:58 +0000

    random message

---
+++ app.js
@@ -0,0 +1,36 @@
+// Random number from zero to ten
+const randomNumber = Math.floor(Math.random() * 4);
+
+//assigning the numbers to a quote
+
+switch (randomNumber) {
+    case 0:
+        console.log('When we are absolutely sure we are right, we can be absolutely sure we are wrong');
+    break;
+
+    case 1:
+        console.log('The truth is not a stick with which to beat yourself')
+    break;
+
+    case 2:
+        console.log('Forgiveness is an act of willingness not an act of will');
+        break;
+
+    case 3:
+         console.log('If we hold onto our wounds in an area of life then we hold back the growth of wisdom in that area too');
+         break; 
+            
+    case 4:
+         console.log('We need to be willing to accept our feelings around an event in order to fully gain the benefits, which are available from it');
+        break;
+
+
+
+}   
+
+
+
+
+
+
+
